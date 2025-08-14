e49e1b7f2b8cedde719da2207a581442c2381ed9
danilyn-baquilar <danicabaquilar01@gmail.com>
2025-08-15 00:47:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+VANTA.BIRDS({
+  el: "#background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0xd9d6d6,
+  color1: 0x90505,
+  color2: 0x90409,
+  colorMode: "variance",
+  birdSize: 1.30,
+  wingSpan: 15.00,
+  separation: 11.00,
+  quantity: 3.00
+})
